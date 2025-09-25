f247370ce3aa39a2b2fb5757d9d57030da15059d
Rohith-Parambath <rohithparambath@gmail.com>
2025-09-25 12:51:30 +0530

    project scaffold

---
+++ src/test/java/com/rohith/TaskManagem/TaskManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohith.TaskManagem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
